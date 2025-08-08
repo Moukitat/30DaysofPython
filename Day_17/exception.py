eb5fe9b9899f5c49f71973a8318cddf550b8eb93
moukitatlassisi6@gmail.com <moukitatlassisi6@gmail.com>
2025-08-08 14:23:29 +0000

    exception

---
+++ Day_17/exception.py
@@ -0,0 +1,11 @@
+#Exercice jour 17
+
+names = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+
+nordic_countries = names[:5]
+
+ES, RU = names[5], names[6]
+
+print("Nordic countries:", nordic_countries)
+print("Estonia:", ES)
+print("Russia:", RU)
